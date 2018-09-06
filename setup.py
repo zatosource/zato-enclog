7f77d01889f318df66af16916a46657ba3e722cb
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-09-06 16:02:30 +0200

    Updating copyright year.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2015 Dariusz Suchojad <dsuch at zato.io>
+Copyright (C) 2018, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
+
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 # Originally part of Zato - open-source ESB, SOA, REST, APIs and cloud integrations in Python
 # https://zato.io
--- src/zato/enclog/__init__.py
+++ src/zato/enclog/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""
+Copyright (C) 2018, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
 from __future__ import absolute_import, division, print_function, unicode_literals
-
-"""
-Copyright (C) 2015 Dariusz Suchojad <dsuch at zato.io>
-
-Licensed under BSD, see LICENSE.txt for terms and conditions.
-"""
 
 # Part of Zato - Open-source ESB, SOA, REST, APIs and Cloud Integrations in Python
 # https://zato.io
